dbfba870d3850d6fcbef667b4b3478b7bfcfb673
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/InfiniteScrollContrib@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/InfiniteScrollContrib.pm
+++ lib/Foswiki/Contrib/InfiniteScrollContrib.pm
@@ -3,14 +3,15 @@ package Foswiki::Contrib::InfiniteScrollContrib;
 use strict;
 use warnings;
 
-our $VERSION = '$Rev$';
-our $RELEASE = '1.11';
-our $SHORTDESCRIPTION = 'Infinite scrolling layouts';
+our $VERSION           = '$Rev$';
+our $RELEASE           = '1.11';
+our $SHORTDESCRIPTION  = 'Infinite scrolling layouts';
 our $NO_PREFS_IN_TOPIC = 1;
 
 sub init {
-  require Foswiki::Plugins::JQueryPlugin;
-  Foswiki::Plugins::JQueryPlugin::registerPlugin("InfiniteScroll", "Foswiki::Contrib::InfiniteScrollContrib::Core");
+    require Foswiki::Plugins::JQueryPlugin;
+    Foswiki::Plugins::JQueryPlugin::registerPlugin( "InfiniteScroll",
+        "Foswiki::Contrib::InfiniteScrollContrib::Core" );
 }
 
 1;
--- lib/Foswiki/Contrib/InfiniteScrollContrib/Core.pm
+++ lib/Foswiki/Contrib/InfiniteScrollContrib/Core.pm
@@ -7,22 +7,23 @@ use Foswiki::Plugins::JQueryPlugin::Plugin ();
 our @ISA = qw( Foswiki::Plugins::JQueryPlugin::Plugin );
 
 sub new {
-  my $class = shift;
+    my $class = shift;
 
-  my $this = bless(
-    $class->SUPER::new(
-      name => 'InfiniteScroll',
-      version => '2.0e159f28de22ee386baa2',
-      author => 'Paul Irish, Luke Shumard and Michael Daum',
-      homepage => 'http://www.infinite-scroll.com',
-      javascript => ['jquery.infinitescroll.js', 'jquery.infinitescroll.init.js'], 
-      documentation => 'InfiniteScrollContrib',
-      puburl => '%PUBURLPATH%/%SYSTEMWEB%/InfiniteScrollContrib',
-    ),
-    $class
-  );
+    my $this = bless(
+        $class->SUPER::new(
+            name     => 'InfiniteScroll',
+            version  => '2.0e159f28de22ee386baa2',
+            author   => 'Paul Irish, Luke Shumard and Michael Daum',
+            homepage => 'http://www.infinite-scroll.com',
+            javascript =>
+              [ 'jquery.infinitescroll.js', 'jquery.infinitescroll.init.js' ],
+            documentation => 'InfiniteScrollContrib',
+            puburl        => '%PUBURLPATH%/%SYSTEMWEB%/InfiniteScrollContrib',
+        ),
+        $class
+    );
 
-  return $this;
+    return $this;
 }
 
 1;
