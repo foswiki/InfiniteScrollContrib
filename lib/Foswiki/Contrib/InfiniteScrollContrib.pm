aa5054ea229639ce0d997f4f78c5d3d2ab2abd5c
MichaelDaum <daum@michaeldaumconsulting.com>
2018-08-16 11:02:57 +0200

    Item14751: maintenance and docu
    make sure this is still releasable

--- lib/Foswiki/Contrib/InfiniteScrollContrib.pm
+++ lib/Foswiki/Contrib/InfiniteScrollContrib.pm
@@ -1,16 +1,27 @@
+# Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+#
+# Copyright (C) 2011-2018 Foswiki Contributors
+#
+# For licensing info read LICENSE file in the Foswiki root.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details, published at 
+# http://www.gnu.org/copyleft/gpl.html
+
 package Foswiki::Contrib::InfiniteScrollContrib;
 
 use strict;
 use warnings;
 
-our $VERSION = '1.13';
-our $RELEASE = '1.13';
+our $VERSION = '1.14';
+our $RELEASE = '16 Aug 2018';
 our $SHORTDESCRIPTION = 'Infinite scrolling layouts';
 our $NO_PREFS_IN_TOPIC = 1;
-
-sub init {
-  require Foswiki::Plugins::JQueryPlugin;
-  Foswiki::Plugins::JQueryPlugin::registerPlugin("InfiniteScroll", "Foswiki::Contrib::InfiniteScrollContrib::Core");
-}
 
 1;
